cbef808b74f6a9d04178d14eb09fa5deff5fa8c9
NNADIHENRY <nnadihenry92@gmail.com>
2018-12-05 15:05:59 +0000

    henry commit

---
+++ app/src/main/java/com/example/henry/abaglobal/TermsAndConditions.java
@@ -0,0 +1,13 @@
+package com.example.henry.abaglobal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TermsAndConditions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.terms_and_conditions);
+    }
+}
