052037e4b0e57aa5da2f0555b2f30a33b14636a6
pryhmez <edmundisaac16@gmail.com>
2018-12-05 15:46:34 +0100

    pryhmez 3rd commit

--- app/src/main/java/com/example/henry/abaglobal/SplashScreen.java
+++ app/src/main/java/com/example/henry/abaglobal/SplashScreen.java
@@ -1,5 +1,6 @@
 package com.example.henry.abaglobal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,27 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Thread timer = new Thread(){
+            public void run(){
+                try {
+                    sleep(2000);
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(new Intent(SplashScreen.this, RegisterationActivity.class));
+                }
+            }
+        };timer.start();
+
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
 }
